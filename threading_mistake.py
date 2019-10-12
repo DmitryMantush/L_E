b96df7fd5c0f60ff85a844c5809797991dedda1d
DmitryMantush <brownmittens@gmail.com>
2019-10-12 20:44:17 +0300

    'a' now is a sum result of all the threads.

--- threading_mistake.py
+++ threading_mistake.py
@@ -1,23 +1,30 @@
 # Найти и исправить ошибку (см файл task3.py), оставив многопоточность.
-from threading import Thread
+import concurrent.futures
+
+a = 0
 
 
-def function(arg):
-    a = 0
+def function(arg: int):
+    n = 0
     for i in range(arg):
-        a += 1
-    print("----------------------", a)
+        n += 1
+    return n  # result of single thread incrementation
 
 
 def main():
-    threads = []
-    for i in range(5):
-        thread = Thread(target=function, args=(1000000,))
-        thread.start()
-        threads.append(thread)
-
-    [t.join() for t in threads]
-    # print("----------------------", a)  # a is a result of each thread
+    with concurrent.futures.ThreadPoolExecutor() as tpe:
+        results = [tpe.submit(function, 1000000) for _ in range(5)]
+        for z in concurrent.futures.as_completed(results):
+            global a
+            a += z.result()
+    # threads = []
+    # for _ in range(5):
+    #     thread = Thread(target=function, args=(1000000,))
+    #     thread.start()
+    #     threads.append(thread)
+    #
+    # [t.join() for t in threads]
+    print("----------------------", a)  # a is sum result of all the threads
 
 
 main()
